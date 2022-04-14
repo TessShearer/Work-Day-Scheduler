8c565aea75994c6f1fbed6d8e4bca467c84006da
TessShearer <shearer.tess@gmail.com>
2022-04-13 18:18:10 -0600

    add save and typing capabilities

--- Develop/script.js
+++ Develop/script.js
@@ -1,64 +1,69 @@
 // Puts date and time at the top of the page
-var todaysDate = new Date();
+var todaysDate = moment().format('YYYY-MM-DD');
+var currentHour = moment().format("H")
 var currentDay = document.querySelector("#currentDay");
-currentDay.innerText=(todaysDate);
+currentDay.innerText = (todaysDate);
 
 // arrays and definitions/variables
-var container=document.querySelector(".container");
-let workingHoursArr = [9,10,11,12,13,14,15,16,17]
+var container = document.querySelector(".container");
+let workingHoursArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
 
 // for loop that calls createRow function 9 times to fill the page with a row for every hour
-for(i=0;i<workingHoursArr.length;i++){
-  createRow();
-  if (todaysDate.getHours() > workingHoursArr[i]) {
-    textArea.classList.add("past")
-  }
-    else if (todaysDate.getHours() == workingHoursArr[i]) {
+for (i = 0; i < workingHoursArr.length; i++) {
+  createRow(workingHoursArr[i]);
+
+
+  // function that creates one of the rows for an hour of the workday
+  function createRow(time) {
+
+    let storageTime = localStorage.getItem(time) || ""
+
+    const row = document.createElement("div");
+    container.appendChild(row);
+    row.classList.add("row")
+    row.classList.add("time-block");
+
+    const rowHour = document.createElement("div");
+    row.appendChild(rowHour);
+    rowHour.classList.add("hour");
+    const iFromForLoop = time;
+    if (iFromForLoop < 10) {
+      rowHour.innerText = ("0" + iFromForLoop + ":00")
+    }
+    else
+    rowHour.innerText = (iFromForLoop + ":00");
+
+    const textArea = document.createElement("textarea");
+    row.appendChild(textArea);
+    textArea.classList.add("textarea");
+    textArea.innerText = storageTime;
+
+    const saveBtn = document.createElement("div");
+    row.appendChild(saveBtn);
+    saveBtn.classList.add("saveBtn");
+    saveBtn.innerText = ("SAVE  ");
+
+    if (currentHour > workingHoursArr[i]) {
+      textArea.classList.add("past")
+    }
+    else if (currentHour == workingHoursArr[i]) {
       textArea.classList.add("present")
     }
-      else {
-        textArea.classList.add("future")
+    else {
+      textArea.classList.add("future")
+    };
+
+        saveBtn.addEventListener("click", function (event) {
+          localStorage.setItem(time,event.target.previousElementSibling.value)
+        }
+        )
   };
-
-// function that creates one of the rows for an hour of the workday
-function createRow() {
-  const row = document.createElement("div");
-  container.appendChild(row);
-  row.classList.add("row");
-
-  const rowHour = document.createElement("div");
-  row.appendChild(rowHour);
-  rowHour.classList.add("hour"); 
-  const iFromForLoop = "9";
-  rowHour.innerText=(iFromForLoop + ":00");
-
-  const textArea = document.createElement("div");
-  row.appendChild(textArea);
-  textArea.classList.add("textarea");
-  textArea.innerText=("hey!");
-
-  const saveBtn = document.createElement("div");
-  row.appendChild(saveBtn);
-  saveBtn.classList.add("saveBtn");
- saveBtn.innerText=("SAVE  ");
-
-
-  saveBtn.addEventListener("click", function (){
-    
-  }
-  )};
 };
 
-console.log(todaysDate.getHours());
-
-// This is random stuff I tried but does not work currently lol
-var time = workingHoursArr[(todaysDate.getHours) - 8];
-console.log(JSON.stringify(time));
-
+console.log(moment().toDate())
 
 // Interval that refreshes time elements every few minutes
 setInterval(function () {
-  var todaysDate = new Date();
-  currentDay.innerText=(todaysDate);
-  console.log("hey!");
-  }, 60000);
+  document.location.reload();
+}, 300000);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+# <Work-Day-Scheduler>
+
+## Description
+
+Employees with busy schedule need a way to organize and prioritize their workdays. I have created a scheduler that shows a text area for each hour of the workday. It allows employees to type and save tasks that need to be done by that time. The current time is highlighted red to show a visual warning of the items that need to be finished up. Future times are green, and past times are white. Employees can leave and revisit the page and events will remain saved.
+
+## Usage
+
+Visit the finished product here: https://tessshearer.github.io/Password-Generator/
+
+![Screenshot of Password Generator](./assets/images/password-generator-screenshot.png)
+
+
+## Credits
+
+I created and finished this project on my own.# Password Generator Starter Code
+
+
+
